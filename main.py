e9e3480e598f298a8c9a0c0ea0e5f60f0f0790bf
Amanda <115050592+polarolo@users.noreply.github.com>
2025-04-08 15:59:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,27 @@
 from vedo import *
 
 
-world = Box([0,0,0], 30, 16, 16).wireframe()
+world = Box([0,0,0], 35, 16, 16).wireframe()
 
-plane1 = Mesh(dataurl+"cessna.vtk").c("green")
-plane1.add_trail(n=100)
-plane1.add_shadow('z', -4).add_shadow('y', 8)
+board = Mesh("Dartboard.stl").c("red")
+board.scale(.03)
+board.rotate(90, (0, 0, 1))
+board.pos(17, 0, 0)
+
+dart = Mesh("Dart.stl").c("green")
+dart.scale(0.03)  # Adjust this number to make the model smaller/larger
+dart.rotate(-90, (0, 1, 0))  
+dart.add_trail(n=100)
+dart.add_shadow('y', -8)
 
 
 # Setup the scene
 plt = Plotter(interactive=False)
-plt.show(world, plane1, __doc__, viewup="-x")
+plt.show(world, dart, board, __doc__, viewup="-y")
 
 for t in np.arange(0, 3.2, 0.04):
-    plane1.pos(9*t-15, 2-t, sin(3-t)) # movement
-    plane1.update_trail().update_shadows()
+    dart.pos(9*t-15, sin(3-t), 2-t) # movement
+    dart.update_trail().update_shadows()
     plt.render()
 
 
